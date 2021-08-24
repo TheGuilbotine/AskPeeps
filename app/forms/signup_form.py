07a07ebc08a6743b33eaeafcad46b270d91932b8
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-24 19:08:59 +0200

    add questions model

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, DateField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -24,4 +24,7 @@ class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
+    f_name = StringField('f_name', validators=[DataRequired()])
+    l_name = StringField('l_name', validators=[DataRequired()])
+    birth_date = DateField('birth_date', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
