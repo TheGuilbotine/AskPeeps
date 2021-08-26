e544bdc115cc21bc633d5db74e672db48c4bea87
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-26 17:32:40 +0200

    navbar start

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,38 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/feed' exact={true} activeClassName='active'>
-            Questions Feed
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+    <div className="navbar__container">
+        <nav className="navbar-links__container">
+            <div className="navbar-link__container">
+                <NavLink className="navbar-link" to='/' exact={true} activeClassName='active'>
+                    Home
+                </NavLink>
+            </div>
+            <div className="navbar-link__container">
+                <NavLink className="navbar-link" to='/feed' exact={true} activeClassName='active'>
+                    Questions Feed
+                </NavLink>
+            </div>
+            <div className="navbar-link__container">
+                <NavLink className="navbar-link" to='/login' exact={true} activeClassName='active'>
+                    Login
+                    </NavLink>
+            </div>
+            <div className="navbar-link__container">
+                <NavLink className="navbar-link" to='/sign-up' exact={true} activeClassName='active'>
+                    Sign Up
+                </NavLink>
+            </div>
+            <div className="navbar-link__container">
+                <LogoutButton className="navbar-button"/>
+            </div>
+        </nav>
+    </div>
+    );
 }
 
 export default NavBar;
