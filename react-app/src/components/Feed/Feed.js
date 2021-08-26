605a1a391005cb15038650c89df4753f74c5d98f
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-27 01:31:01 +0200

    start feed css

--- react-app/src/components/Feed/Feed.js
+++ react-app/src/components/Feed/Feed.js
@@ -29,25 +29,29 @@ export default function FeedPage() {
     return (
         <div>
             {/* <QuestionSideBar /> */}
-            <QuestionForm />
-            <h1>Hello {sessionUser?.username} welcome to the Questions Feed</h1>
-            {questions && questions?.map((question, idx) => (
-                <div key={idx}>
-                    {question?.username}: {question.question}
-                    <button className="delete-confirmation-button" onClick={() => dispatch(destroyQuestion(question.id))}>Delete</button>
-                    <EditQuestion questionId={question?.id} />
-                    {question.responses && question.responses?.map((response, idx) => (
-                        <div>
-                            {response?.username} said, {response.response}
-                            <button className="delete-confirmation-button" onClick={() => dispatch(destroyResponse(response.id))}>Delete</button>
-                            <EditResponse responseId={response?.id} questionId={question?.id} />
-                        </div>
-                    ))}
-                    {sessionUser && <ResponseForm questionId={question?.id} />}
-                    {/* <QuestionResponses questionId={question?.id} /> */}
-                    {/* <DeleteQuestionModal questionId={question?.id} /> */}
-                </div>
-            ))}
+            <div className="feed-title__container">
+                <h1>Hello {sessionUser?.username} welcome to the Questions Feed</h1>
+                <QuestionForm />
+            </div>
+            <div className="questions-feed__container">
+                {questions && questions?.map((question, idx) => (
+                    <div key={idx}>
+                        {question?.username}: {question.question}
+                        <button className="delete-confirmation-button" onClick={() => dispatch(destroyQuestion(question.id))}>Delete</button>
+                        <EditQuestion questionId={question?.id} />
+                        {question.responses && question.responses?.map((response, idx) => (
+                            <div>
+                                {response?.username} said, {response.response}
+                                <button className="delete-confirmation-button" onClick={() => dispatch(destroyResponse(response.id))}>Delete</button>
+                                <EditResponse responseId={response?.id} questionId={question?.id} />
+                            </div>
+                        ))}
+                        {sessionUser && <ResponseForm questionId={question?.id} />}
+                        {/* <QuestionResponses questionId={question?.id} /> */}
+                        {/* <DeleteQuestionModal questionId={question?.id} /> */}
+                    </div>
+                ))}
+            </div>
 
         </div>
     )
