f43c0db8fb015532121a9b9609bcc96714d8e0fb
Pierre Guilbault <guilbaultpj@gmail.com>
2021-08-30 19:45:51 +0200

    demo login errors fixed

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -21,20 +21,11 @@ const LoginForm = () => {
     }
   };
 
-  const demoLogin = async () => {
+  const demoLogin = async (e) => {
+    e.preventDefault();
     await dispatch(login('demo@aa.io', 'password'))
     alert(`Welcome back to AskPeeps.`)
   }
-
-  // const demoLogin = async () => {
-  //   const data = await dispatch(login('demo@aa.io', 'password'));
-  //   if (data) {
-  //     setErrors(data);
-  //   } else {
-  //     alert(`Welcome back to AskPeeps.`)
-  //   }
-  // }
-
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
